8c07927cc54f70e21c8aab5241da13e0e0b2dc09
ebukathedev <ebuskelele@gmail.com>
2023-08-25 10:47:24 +0100

    fix button hover

--- src/UI/Button.jsx
+++ src/UI/Button.jsx
@@ -1,22 +1,24 @@
 /* eslint-disable react/prop-types */
 const buttonStyles =
-	"relative inline-block transition-all duration-300 ease-in-out delay-100 border rounded-md border-green hover:-translate-x-1 hover:-translate-y-1 text-green font-mono";
+	"relative block transition-all duration-300 ease-in-out delay-100 border rounded-md border-green hover:-translate-x-1 hover:-translate-y-1 text-green font-mono";
 
 const Button = ({ children, href, className, bgColor }) => {
 	return (
-		<div className="relative inline-flex -z-10">
-			<span className="absolute inset-0 rounded-md bg-green"></span>
-			<a
+		<a
+			className="relative focus:outline-none"
+			href={href}
+			target="_blank"
+			rel="noreferrer"
+		>
+			<span
 				className={`${buttonStyles} ${className || "px-4 py-2"} ${
 					bgColor || "bg-navy-green"
-				}`}
-				href={href}
-				target="_blank"
-				rel="noreferrer"
+				}  `}
 			>
 				{children}
-			</a>
-		</div>
+			</span>
+			<span className="absolute -z-10 inset-0 rounded-md bg-green"></span>
+		</a>
 	);
 };
 
