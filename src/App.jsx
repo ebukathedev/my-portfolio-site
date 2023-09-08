f9b84f0b0c7e93915b28b042300a06db92b560d9
ebukathedev <ebuskelele@gmail.com>
2023-09-08 22:16:28 +0100

    stash preloader code

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,13 @@ import Contact from "./containers/Contact";
 import Footer from "./containers/Footer";
 import Hero from "./containers/Hero";
 import Navbar from "./containers/Navbar";
-import PreLoader from "./containers/PreLoader";
 import Projects from "./containers/Projects";
 
 const App = () => {
-	const [isLoading, setIsLoading] = useState(false);
 	const [isOpen, setIsOpen] = useState(false);
 	const toggleMenu = () => {
 		setIsOpen((isOpen) => !isOpen);
 	};
-
-	// useEffect(() => {
-	// 	const showPreLoader = setTimeout(() => {
-	// 		setIsLoading(false);
-	// 	}, 1000);
-
-	// 	return () => clearInterval(showPreLoader);
-	// }, []);
 
 	useEffect(() => {
 		if (isOpen) {
@@ -34,22 +24,18 @@ const App = () => {
 		};
 	}, [isOpen]);
 
-	if (isLoading) {
-		return <PreLoader />;
-	} else {
-		return (
-			<>
-				<Navbar toggleMenu={toggleMenu} isOpen={isOpen} />
-				<main className="px-6 sm:px-12 md:px-24 lg:px-36 max-w-[1600px] min-h-screen mx-auto">
-					<Hero />
-					<About />
-					<Projects />
-					<Contact />
-				</main>
-				<Footer />
-			</>
-		);
-	}
+	return (
+		<>
+			<Navbar toggleMenu={toggleMenu} isOpen={isOpen} />
+			<main className="px-6 sm:px-12 md:px-24 lg:px-36 max-w-[1600px] min-h-screen mx-auto">
+				<Hero />
+				<About />
+				<Projects />
+				<Contact />
+			</main>
+			<Footer />
+		</>
+	);
 };
 
 export default App;
--- src/containers/PreLoader.jsx
+++ src/containers/PreLoader.jsx
@@ -1,6 +1,20 @@
 import { motion } from "framer-motion";
 import { AiOutlineRocket } from "react-icons/ai";
 
+// When ready to add a preloader put this code in App.js
+// import PreLoader from "./containers/PreLoader";
+// const [isLoading, setIsLoading] = useState(false);
+// useEffect(() => {
+// 	const showPreLoader = setTimeout(() => {
+// 		setIsLoading(false);
+// 	}, 1000);
+
+// 	return () => clearInterval(showPreLoader);
+// }, []);
+// if (isLoading) {
+// 	return <PreLoader />;
+// }
+
 const PreLoader = () => {
 	return (
 		<div className="h-screen bg-dark-navy-green text-white relative">
