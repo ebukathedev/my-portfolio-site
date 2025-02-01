3f6d05e071accf0700f39707af6a1d384bc2eca8
Ebuka Ejiofor <89203154+ebukathedev@users.noreply.github.com>
2025-02-01 10:27:24 +0100

    Update ResumeButton.jsx

--- src/components/ResumeButton.jsx
+++ src/components/ResumeButton.jsx
@@ -4,7 +4,7 @@ import ButtonLink from "../UI/ButtonLink";
 const ResumeButton = ({ className, bgColor }) => {
 	return (
 		<ButtonLink
-			href="https://drive.google.com/file/d/1JOm7KpCmZXlVJh9ArY84RGOcsvpClnjD/view?usp=sharing"
+			href="https://drive.google.com/file/d/1FKqXBy_qhW6tlLVWn73EldR62M9aLXqt/view?usp=sharing"
 			className={className}
 			bgColor={bgColor}
 		>
